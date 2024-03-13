d1ab0528dcf62c39f23741a5737347a0ce1cf4ed
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-03-13 21:00:23 +0200

    done

---
+++ ClassLibrary4/Facade.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Facade
+{
+    private Subsystem1 _subsystem1;
+    private Subsystem2 _subsystem2;
+
+    public Facade()
+    {
+        _subsystem1 = new Subsystem1();
+        _subsystem2 = new Subsystem2();
+    }
+
+    public void Operation1()
+    {
+        _subsystem1.Operation1();
+        _subsystem2.Operation1();
+    }
+
+    public void Operation2()
+    {
+        _subsystem1.Operation2();
+        _subsystem2.Operation2();
+    }
+}
---
+++ ClassLibrary4/Program2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+public class Client
+{
+    public static void Main(string[] args)
+    {
+        Console.InputEncoding = Encoding.Unicode;
+        Console.OutputEncoding = Encoding.Unicode;
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.CursorVisible = false;
+        Facade facade = new Facade();
+
+        facade.Operation1();
+        facade.Operation2();
+
+        Console.ReadKey();
+    }
+}
